14ab2e351630533e161d910ecd6959f84b669ea2
Svetlana <svetlana_el12@mail.ru>
2022-05-29 23:59:09 +0500

    3.arrays/task.js\

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,11 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
+  let result = (arr1.length === arr2.length) && arr1.every((item, ind) => item === arr2[ind]);
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  let result = arr.filter((item) => item > 0 && item % 3 === 0).map((item) => item * 10)
 
-  // Ваш код
-
-  return resultArr; // array
+  return result; // array
 }
