afb897d6b050cf01858756134e1bde7d3dd004b8
Svetlana <svetlana_el12@mail.ru>
2022-06-13 19:26:19 +0500

    4.objects/task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,39 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  name,
+  gender,
+  age
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
+  }
+
+  this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function (...mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
+  }
+  
+  for (let m of mark) {
+    this.marks.push(m);
+  }
+}
+
+Student.prototype.getAverage = function () {
+  let array = this.marks;
+
+  return array.reduce((sum, item) => sum + item, 0) / array.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
